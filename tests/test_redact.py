a6a92e7f227df230eee51edd2095fadc89034b9d
Nithivarn-Reddy <nithivarn.reddy.shanigaram-1@ou.edu>
2020-04-07 02:16:10 +0000

    testcases updated

--- tests/test_redact.py
+++ tests/test_redact.py
@@ -5,18 +5,19 @@ def test_readFiles(pattern=['*.text','otherfiles/*.md']):
     assert readFiles(pattern) is not None
 
 def test_redact_names():
-    data,count=redact_names(["KING GEORGE Martin Luther king"])
-    assert len(data) != 0 and len(count) !=0
+    data,count=redact_names(["Martin Luther king."])
+    assert len(data) != 0 and count[0] == 2
 
 def test_redact_dates():
-    assert redact_dates(['09/09/2020','05/25/1995']) is not None
+    data,count = redact_dates(['09/09/2020','05/25/1995'])
+    assert len(data) !=0 and len(count) == 2
 
 def test_redact_genders():
-    data,count=redact_genders(['he she man woman'])
-    assert len(data) != 0 and len(count) !=0
+    data,count=redact_genders(['he she man woman.'])
+    assert len(data) != 0 and count[0] == 4
 
 def test_redact_concept():
-    data,sentences,count=redact_concept(['study school university post graduate'],'study')
+    data,sentences,count=redact_concept(['study school university post graduate.'],'study')
     assert len(data)!=0 and len(sentences)!=0 and len(count)!=0
 
 def test_redact_stats(capsys):
